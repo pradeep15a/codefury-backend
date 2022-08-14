0adf7d556544fd195c5380a6647b1f637c8abc9b
PRADEEP  A <pradeepashok1122@gmail.com>
2022-08-14 05:45:59 +0530

    clear-working

--- controllers/projects.js
+++ controllers/projects.js
@@ -17,30 +17,8 @@ export const fetchProjects = async (req, res) => {
     try {
         const projects = await ProjectData.find();
 
-        res.status(200).json({ data : projects });
+        res.status(203).json(projects);
     } catch (error) {
         res.status(404).json({ message : error.message });
     }
-}
-
-export const invest = async (req, res) => {
-    const { id } = req.params;
-
-    if (!req.userId) return res.send(404).json({ message: "Unauthenticated" })
-
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No project with id: ${id}`);
-
-    const project = await ProjectData.findById(id);
-
-    const index = project.investors.findIndex((id) => id === String(req.name));
-
-    if (index === -1) {
-        project.investors.push(req.name);
-    } 
-    else {
-        project.investors = project.investors.filter((id) => id !== String(req.name))
-    }
-    const updatedProject = await PostMessage.findByIdAndUpdate(id, project, { new: true });
-
-    res.json(updatedProject);
 }
--- routes/project.js
+++ routes/project.js
@@ -1,13 +1,11 @@
 import express from 'express';
 
-import { fetchProjects, createProject, invest } from '../controllers/projects.js'; 
+import { fetchProjects, createProject } from '../controllers/projects.js'; 
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', fetchProjects);
 router.post('/', createProject);
-router.patch('/:id/invest', auth, invest)
-
 
 export default router;
